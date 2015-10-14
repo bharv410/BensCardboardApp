86dfe76b57ce05408d260bf6c944893f22bfaa9c
bharv410 <benjamin.harvey@globant.com>
2015-10-14 13:52:37 -0700

    first commit working

---
+++ app/src/main/java/com/kigeniushq/benfirstcardboard/MainActivity.java
@@ -0,0 +1,32 @@
+package com.kigeniushq.benfirstcardboard;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+
+import org.rajawali3d.cardboard.RajawaliCardboardRenderer;
+import org.rajawali3d.cardboard.RajawaliCardboardView;
+
+public class MainActivity extends CardboardActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RajawaliCardboardView view = new RajawaliCardboardView(this);
+        setContentView(view);
+        setCardboardView(view);
+
+        RajawaliCardboardRenderer renderer = new MyRenderer(this); // your renderer
+        view.setRenderer(renderer);        // required for CardboardView
+        view.setSurfaceRenderer(renderer); // required for RajawaliSurfaceView
+    }
+}
